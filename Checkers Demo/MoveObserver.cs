937835ef4bb52195298b7a4ec316815cde4512d4
Thomas Micol <thomasmicol@gmail.com>
2018-02-23 22:02:18 +1300

    Final commit for the day. made the concrete MoveObserver Class. This will house the logic for checking the board if there is possible moves or if specified moves are possible.

---
+++ Checkers Demo/MoveObserver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers_Demo
+{
+    class MoveObserver : IMoveObserver
+    {
+        public List<IMove> GetPossibleMoves(ILevelData levelData)
+        {
+            
+            foreach()
+            {
+
+            }
+        }
+
+        public bool IsMovePossible(IMove aMove)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
