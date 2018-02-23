ad23dcc709b04a17d357f080a37e823c7b3e6237
Thomas Micol <thomasmicol@gmail.com>
2018-02-23 21:53:05 +1300

    Added the IMoveObserver interface, this defines the signatures needed for any class that implements this interface. The purpose of that group of classes will be to check if moves are possible and get sets of possible moves from the board when it finds them.

---
+++ Checkers Demo/IMoveObserver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers_Demo
+{
+    interface IMoveObserver
+    {
+        List<Move> GetPossibleMoves(ILevelData levelData);
+        bool IsMovePossible(IMove aMove);
+    }
+}
